cd921c75538221766e4e1f21012dde228708133e
Hamim <hamimc232@gmail.com>
2025-06-12 19:42:29 -0400

    Initialized project basics. Installed all dependencies for now.

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import { Box, Typography } from "@mui/material";
+
+export default function BasicHomePage() {
+    return (
+        <Box>
+            <Typography>Hello basic test</Typography>
+        </Box>
+        
+    )
+}
