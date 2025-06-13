a671dce4d3201ae4f42eb9ed769c34a12fb33453
Hamim <hamimc232@gmail.com>
2025-06-12 22:55:37 -0400

    minor changes to layout + made other pages

---
+++ app/map/page.js
@@ -0,0 +1,21 @@
+import {
+  Box,
+  Typography,
+  Button,
+  Drawer,
+  Snackbar,
+  Grid,
+  Container,
+  Icon,
+  IconButton,
+} from "@mui/material";
+
+export default function MainMapPage() {
+  return (
+    <>
+      <Box>
+        <Typography>Render map here.</Typography>
+      </Box>
+    </>
+  );
+}
